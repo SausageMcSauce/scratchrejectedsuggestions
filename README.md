32ab455f966f41b4d4f8f63c2d8e9e942dc5da21
SausageMcSauce <69243388+SausageMcSauce@users.noreply.github.com>
2020-08-21 08:28:42 +0000

    made a start

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 
 Some tool to display items from Scratch's [official list of rejected suggestions](https://scratch.mit.edu/discuss/topic/343602/), including BBCode, that can be copied and pasted into Scratch forum posts.
 
+All contents of the list were written by the creator of that forum topic.
+
 Documentation coming soon.
---
+++ list.py
@@ -0,0 +1,26 @@
+# quote template:
+# [quote="The Official List of Rejected Suggestions"]
+# [/quote]
+
+tolorslist = {
+  "1.1": """[quote="The Official List of Rejected Suggestions"][b]1.1 [url=https://scratch.mit.edu/discuss/topic/73978/?page=1#post-634620]"Broadcast received" boolean block[/url][/b]
+This would allow a project to detect when a broadcast is sent.  But, there is a lot of ambiguity on how this would work.  Would it return true if the broadcast was fired since the project was created, since the green flag was clicked, or since something else was broadcasted?  The workaround is simple: use variables that change when a broadcast is received, then use the "equals" block.
+
+However, the blocks "repeat until broadcast received" and "wait until broadcast received" are NOT rejected; you can discuss them on [url=https://scratch.mit.edu/discuss/topic/324844/]this topic[/url].
+
+[scratchblocks]
+<[message v] received? :: events>
+[/scratchblocks]
+[/quote]
+""",
+ "1.2": """[quote="The Official List of Rejected Suggestions"]
+ [b]1.2 [url=https://scratch.mit.edu/discuss/topic/205920/?page=2#post-2070854]"When stop sign clicked" hat block[/url][/b]
+This block would allow users to click the stop sign to run a script.  However, the stop sign is designed to stop all the scripts in the project.  With this block in place, more scripts will start when you want the project to stop, thus defeating the purpose of the stop sign.  This could also be rather confusing for the "stop all" block.  Regardless, there is a workaround that uses the "when (timer) > (number)" hat block; one such workaround can be found in [url=https://scratch.mit.edu/discuss/post/3547555/]this post[/url].
+
+[scratchblocks]
+when stop sign clicked :: events :: hat
+[/scratchblocks]
+
+[/quote]
+"""
+}
